c55fca4bb7eab837246ef521cc75cd71486fc07f
tearexyf <tearexm@gmail.com>
2026-07-01 14:51:07 +0300

    1

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import CreateTask from "../pages/CreateTask/CreateTask";
 import StartPage from "../pages/StartPage/StartPage";
-import Home from "../pages/Home/Home";
 import CreateProfile from "../pages/CreateProfile/CreateProfile";
 import PetProfile from "../pages/PetProfile/PetProfile";
 import Calendar from "../pages/Calendar/Calendar";
@@ -10,13 +9,14 @@ import Setting from "../pages/Setting/Setting";
 import GalleryHub from "../pages/Gallery/GalleryHub";
 import PetGallery from "../pages/Gallery/PetGallery";
 import EditProfile from "../pages/EditProfile/EditProfile";
+import PetsList from "../pages/PetsList/PetsList";
 
 function RouterMy() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<StartPage />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<PetsList />} />
         <Route path="/pet-profile" element={<PetProfile />} />
         <Route path="/calendar" element={<Calendar />} />
         <Route path="/gallery" element={<GalleryHub />} />
